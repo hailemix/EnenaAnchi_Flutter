fdac9204626a87863d7b7f1d51eaed883f0782e3
hailemix <hailemix2@gmail.com>
2019-07-25 20:35:59 +0300

    About Us Animation is added

--- lib/about_us.dart
+++ lib/about_us.dart
@@ -1,18 +1,164 @@
-
+import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:simple_animations/simple_animations.dart';
 
-class AboutUs extends StatefulWidget {
+class FancyBackgroundApp extends StatelessWidget {
   @override
-  _AboutUsState createState() => _AboutUsState();
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Positioned.fill(child: AnimatedBackground()),
+        onBottom(AnimatedWave(
+          height: 180,
+          speed: 1.0,
+        )),
+        onBottom(AnimatedWave(
+          height: 120,
+          speed: 0.9,
+          offset: pi,
+        )),
+        onBottom(AnimatedWave(
+          height: 220,
+          speed: 1.2,
+          offset: pi / 2,
+        )),
+        Center(
+          child:Container(
+          width: 250,
+          height: 250,
+          decoration: BoxDecoration(
+            
+            border: Border.all(style: BorderStyle.solid,color: Colors.white),
+            shape: BoxShape.rectangle,
+            borderRadius: BorderRadius.only(topLeft: Radius.circular(15.0),bottomRight: Radius.circular(15.0))
+            ) ,
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                myTextWidget('Developer',40.0),
+                myTextWidget('AFC Ethiopia', 30.0),
+                myTextWidget('hailemix2@gmail.com', 20.0)
+              ],
+            ),
+          ),
+        ),
+        ]
+    );
+  }
+
+ myTextWidget(String child, double myFontSize) {
+  return Material(
+    type: MaterialType.transparency,
+      child: Padding(
+      padding: const EdgeInsets.all(2.0),
+      child: Text(
+        child,
+        style: TextStyle(fontSize:myFontSize,color: Colors.white),
+      ),
+    ),
+  );
+ }
+  onBottom(Widget child) => Positioned.fill(
+        child: Align(
+          alignment: Alignment.bottomCenter,
+          child: child,
+        ),
+      );
+
+   
 }
 
-class _AboutUsState extends State<AboutUs> {
+class AnimatedWave extends StatelessWidget {
+  final double height;
+  final double speed;
+  final double offset;
+
+  AnimatedWave({this.height, this.speed, this.offset = 0.0});
 
   @override
   Widget build(BuildContext context) {
- 
-  return Container(
-    child: Center(child: Text('Flutter App'),),
-  );
+    return LayoutBuilder(builder: (context, constraints) {
+      return Container(
+        height: height,
+        width: constraints.biggest.width,
+        child: ControlledAnimation(
+            playback: Playback.LOOP,
+            duration: Duration(milliseconds: (5000 / speed).round()),
+            tween: Tween(begin: 0.0, end: 2 * pi),
+            builder: (context, value) {
+              return CustomPaint(
+                foregroundPainter: CurvePainter(value + offset),
+              );
+            }),
+      );
+    });
+  }
+}
+
+class CurvePainter extends CustomPainter {
+  final double value;
+
+  CurvePainter(this.value);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final white = Paint()..color = Colors.white.withAlpha(60);
+    final path = Path();
+
+    final y1 = sin(value);
+    final y2 = sin(value + pi / 2);
+    final y3 = sin(value + pi);
+
+    final startPointY = size.height * (0.5 + 0.4 * y1);
+    final controlPointY = size.height * (0.5 + 0.4 * y2);
+    final endPointY = size.height * (0.5 + 0.4 * y3);
+
+    path.moveTo(size.width * 0, startPointY);
+    path.quadraticBezierTo(
+        size.width * 0.5, controlPointY, size.width, endPointY);
+    path.lineTo(size.width, size.height);
+    path.lineTo(0, size.height);
+    path.close();
+    canvas.drawPath(path, white);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+}
+
+class AnimatedBackground extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final tween = MultiTrackTween([
+      Track("color1").add(Duration(seconds: 3),
+          ColorTween(begin: Color(0xffD38312), end: Colors.lightBlue.shade900)),
+      Track("color2").add(Duration(seconds: 3),
+          ColorTween(begin: Color(0xffA83279), end: Colors.blue.shade600))
+    ]);
+
+    return ControlledAnimation(
+      playback: Playback.MIRROR,
+      tween: tween,
+      duration: tween.duration,
+      builder: (context, animation) {
+        return Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [animation["color1"], animation["color2"]])),
+        );
+      },
+    );
+  }
+}
+
+
+class AboutUs extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return  FancyBackgroundApp();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 
-
-
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
