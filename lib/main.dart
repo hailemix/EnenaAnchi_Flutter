70a01964e98ed03d18724c2a2de307827d4becdc
FU-Developer <hailemix2@gmail.com>
2019-11-03 17:52:12 +0300

    Ads Update
    Banner and Intersitial Ads are updated.

--- lib/main.dart
+++ lib/main.dart
@@ -8,11 +8,12 @@ import 'package:share/share.dart';
 import 'package:flutter/services.dart';
 
 const String MY_APP_ID = "ca-app-pub-9156727777369518~1185879969";
+const String bannerID = "";
+const String interstitialID = "";
 const String EMULATOR_DEVICE = "Pixel 2";
 const String REAL_DEVICE = "ASUS A006";
 void main() {
   FirebaseAdMob.instance.initialize(appId: MY_APP_ID);
-
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
       .then((_) {
     runApp(RomanceApp());
@@ -55,12 +56,13 @@ class MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
   bool isTapped = false;
   String _finalContent = '';
   BannerAd _bannerAd;
-  InterstitialAd _interstitialAd;
+  InterstitialAd _myInterstitial;
   MobileAdTargetingInfo targetingInfo = MobileAdTargetingInfo(
-    keywords: <String>['enena anchi', 'amharic love quotes'],
-    contentUrl: 'httpe://flutter.io',
+    keywords: <String>['enena anchi', 'amharic love quotes','amharic love letters','love letter','amharic romantic quotes'],
+    contentUrl: 'https://play.google.com/store/apps/dev?id=4732824418136294157&hl=en',
     testDevices: <String>[EMULATOR_DEVICE, REAL_DEVICE],
   );
+
   BannerAd createBannerAd() {
     return BannerAd(
         adUnitId: BannerAd.testAdUnitId,
@@ -71,45 +73,30 @@ class MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
         });
   }
 
-  InterstitialAd createInterstitialAd() {
-    return InterstitialAd(
-        adUnitId: InterstitialAd.testAdUnitId,
-        targetingInfo: targetingInfo,
-        listener: (MobileAdEvent event) {
-          print("InterstitialAd event is $event");
-        });
-  }
-
   @override
   void initState() {
     super.initState();
 
-    _bottomIcons = [
-      BottomNavigationBarItem(title: Text('ለአንቺ'), icon: Icon(Icons.face)),
-      BottomNavigationBarItem(title: Text('ለአንተ'), icon: Icon(Icons.mood)),
-      BottomNavigationBarItem(
-          title: Text('ፍቅር'),
-          icon: Icon(Icons.favorite_border),
-          activeIcon: Icon(Icons.favorite_border, color: Colors.red)),
-      BottomNavigationBarItem(
-          title: Text('ስለእኛ'), icon: Icon(Icons.supervised_user_circle)),
-    ];
-
-    _startAnimationController = AnimationController(
-        vsync: this, duration: Duration(milliseconds: 1800));
-    _tapController =
-        AnimationController(vsync: this, duration: Duration(milliseconds: 800));
-    startAnimation = CurvedAnimation(
-        parent: _startAnimationController, curve: Curves.bounceOut);
+    _startAnimationController = AnimationController(vsync: this, duration: Duration(milliseconds: 1800));
+    _tapController = AnimationController(vsync: this, duration: Duration(milliseconds: 800));
+    startAnimation = CurvedAnimation(parent: _startAnimationController, curve: Curves.bounceOut);
     tapAnimation = CurvedAnimation(parent: _tapController, curve: Curves.ease);
     _startAnimationController.forward();
     selectAnimation(startAnimation);
+
     _bannerAd = createBannerAd()
       ..load()
       ..show(
           anchorOffset: 60.0,
-          horizontalCenterOffset: 10.0,
-          anchorType: AnchorType.bottom);
+          horizontalCenterOffset:0.0,
+          anchorType: AnchorType.top);
+
+    _myInterstitial = InterstitialAd(
+        adUnitId: InterstitialAd.testAdUnitId,
+        targetingInfo: targetingInfo,
+        listener: (MobileAdEvent event) {
+
+        });
   }
 
   @override
@@ -133,140 +120,136 @@ class MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
   }
 
   void showInterstitialAd(int pageNumber) {
+    _myInterstitial.load();
 
-      _interstitialAd.load();
-      if (pageNumber % 3 == 0) {
-        _interstitialAd.show(
+    if (pageNumber % 3 == 0) {
+      _myInterstitial.show(
             anchorType: AnchorType.bottom,
             anchorOffset: 0.0,
-            horizontalCenterOffset: 0.0);
-      }
+         );
+    }
+
   }
 
-  @override
-  build(BuildContext context) {
-    return CupertinoTabScaffold(
-      tabBar: CupertinoTabBar(
-        items: _bottomIcons,
-        onTap: (int tappedPage) {
-          if (tappedPage < 3) {
-            isTapped = true;
-            _tapController.forward(from: 0.0);
-            selectAnimation(tapAnimation);
-          }
-        },
-      ),
-      tabBuilder: (BuildContext context, int tabPosition) {
-        return CupertinoTabView(
-          builder: (BuildContext context) {
-            return tabPosition < 3
-                ? Stack(
-                    alignment: Alignment.center,
-                    children: <Widget>[
-                      ContainerWidget(
-                        animation: isTapped ? tapAnimation : startAnimation,
-                        zColor: _containerColor[tabPosition],
-                        theContainer: animationIsCompleted
-                            ? FutureBuilder<JsonContent>(
-                                future: loadContent(),
-                                builder: (context, snapshot) {
-                                  if (snapshot.hasData) {
-                                    return PageView.builder(
-                                      scrollDirection: Axis.horizontal,
-                                      onPageChanged: showInterstitialAd,
-                                      itemBuilder:
-                                          (context, swipePagePosition) {
-                                        switch (tabPosition) {
-                                          case 0:
-                                            _finalContent = snapshot.data
-                                                .contentOne[swipePagePosition];
-                                            break;
-                                          case 1:
-                                            _finalContent = snapshot.data
-                                                .contentTwo[swipePagePosition];
-                                            break;
-                                          case 2:
-                                            _finalContent =
-                                                snapshot.data.contentThree[
-                                                    swipePagePosition];
-                                        }
-                                        return Center(
-                                          child: Padding(
-                                            padding: EdgeInsets.all(25.0),
-                                            child: Text(_finalContent,
-                                                style: TextStyle(
-                                                  fontSize: 35.0,
-                                                  decoration:
-                                                      TextDecoration.none,
-                                                  color: Colors.white,
-                                                )),
-                                          ),
-                                        );
-                                      },
-                                    );
-                                  } else if (snapshot.hasError) {
-                                    return Center(
-                                        child: Text(
-                                            "Fetching data has an error.Details: ${snapshot.error}"));
-                                  }
-                                  return Center(
-                                      child: RefreshProgressIndicator(
-                                          backgroundColor:
-                                              CupertinoColors.white));
-                                })
-                            : Text(''),
-                      ),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: <Widget>[
-                          Padding(
-                            padding:
-                                const EdgeInsets.only(right: 40, bottom: 110),
-                            child: Row(
-                              crossAxisAlignment: CrossAxisAlignment.end,
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: <Widget>[
-                                CupertinoButton(
-                                  child: Icon(
-                                    CupertinoIcons.share,
-                                    color: Colors.white,
-                                    size: 45.0,
-                                  ),
-                                  onPressed: () {
-                                    SystemSound.play(SystemSoundType.click);
-                                    Share.share(_finalContent, subject: 'ፍቅር');
-                                  },
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                      //
-                      /*
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
+    @override
+    build(BuildContext context) {
 
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.only(right: 15,left: 15,bottom: 65),
-                      child: Container(
-                        height: 60,
-                        decoration: BoxDecoration(
-                          color: Colors.black,
-                          borderRadius: BorderRadius.all(Radius.circular(5.0)),
-                        ),
-                        child: Center(child: Text('Banner Ad'),),
-                      ),
-                    ),
-                  ],
-                ) */
-                    ],
-                  )
-                : AboutUs();
+      _bottomIcons = [
+        BottomNavigationBarItem(title: Text('ለአንቺ'), icon: Icon(Icons.face)),
+        BottomNavigationBarItem(title: Text('ለአንተ'), icon: Icon(Icons.mood)),
+        BottomNavigationBarItem(
+            title: Text('ፍቅር'),
+            icon: Icon(Icons.favorite_border),
+            activeIcon: Icon(Icons.favorite_border, color: Colors.red)),
+        BottomNavigationBarItem(
+            title: Text('ስለእኛ'), icon: Icon(Icons.supervised_user_circle)),
+      ];
+
+      return CupertinoTabScaffold(
+        tabBar: CupertinoTabBar(
+          items: _bottomIcons,
+          onTap: (int tappedPage) {
+            if (tappedPage < 3) {
+              isTapped = true;
+              _tapController.forward(from: 0.0);
+              selectAnimation(tapAnimation);
+            }
           },
-        );
-      },
-    );
+        ),
+        tabBuilder: (BuildContext context, int tabPosition) {
+          return CupertinoTabView(
+            builder: (BuildContext context) {
+              return tabPosition < 3
+                  ? Stack(
+                alignment: Alignment.center,
+                children: <Widget>[
+                  ContainerWidget(
+                    animation: isTapped ? tapAnimation : startAnimation,
+                    zColor: _containerColor[tabPosition],
+                    theContainer: animationIsCompleted
+                        ? FutureBuilder<JsonContent>(
+                        future: loadContent(),
+                        builder: (context, snapshot) {
+                          if (snapshot.hasData) {
+                            return PageView.builder(
+                              scrollDirection: Axis.horizontal,
+                              onPageChanged: showInterstitialAd,
+                              itemBuilder:
+                                  (context, swipePagePosition) {
+                                switch (tabPosition) {
+                                  case 0:
+                                    _finalContent = snapshot.data
+                                        .contentOne[swipePagePosition];
+                                    break;
+                                  case 1:
+                                    _finalContent = snapshot.data
+                                        .contentTwo[swipePagePosition];
+                                    break;
+                                  case 2:
+                                    _finalContent =
+                                    snapshot.data.contentThree[
+                                    swipePagePosition];
+                                }
+                                return Center(
+                                  child: Padding(
+                                    padding: EdgeInsets.all(25.0),
+                                    child: Text(_finalContent,
+                                        style: TextStyle(
+                                          fontSize: 35.0,
+                                          decoration:
+                                          TextDecoration.none,
+                                          color: Colors.white,
+                                        )),
+                                  ),
+                                );
+                              },
+                            );
+                          } else if (snapshot.hasError) {
+                            return Center(
+                                child: Text(
+                                    "Fetching data has an error.Details: ${snapshot
+                                        .error}"));
+                          }
+                          return Center(
+                              child: RefreshProgressIndicator(
+                                  backgroundColor:
+                                  CupertinoColors.white));
+                        })
+                        : Text(''),
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      Padding(
+                        padding:
+                        const EdgeInsets.only(right: 40, bottom: 110),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: <Widget>[
+                            CupertinoButton(
+                              child: Icon(
+                                CupertinoIcons.share,
+                                color: Colors.white,
+                                size: 45.0,
+                              ),
+                              onPressed: () {
+                                SystemSound.play(SystemSoundType.click);
+                                Share.share(_finalContent, subject: 'ፍቅር');
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              )
+            : AboutUs();
+            },
+          );
+        },
+      );
+    }
   }
-}
+
